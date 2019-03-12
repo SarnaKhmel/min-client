e8b930382564c1c69f403eb49f81f2b83a21eb84
slowbeam <sedwardscode@gmail.com>
2019-03-12 12:21:52 -0400

    created click handlers for start and stop and linked them to the timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import './Timer.css';
 
 export default class Timer extends Component {
+
+  state = {
+    currentTime: 0,
+    timerEnd: null,
+    intervalNum: null
+
+  }
+
+  handleStartClick = () => {
+    const timer = setInterval(() => {
+      this.setState({
+        currentTime: this.state.currentTime + 1
+      })
+    },
+    1000);
+
+    this.setState({
+      intervalNum: timer
+    })
+  }
+
+  handleStopClick = () => {
+    clearInterval(this.state.intervalNum);
+  }
+
+
   render() {
     return (
       <div className="timer">
-        <div className="timer-counter">00:00</div>
+        <div className="timer-counter">{this.state.currentTime}</div>
         <div className="timer-buttons">
-            <div className="timer-button start">start</div>
-            <div className="timer-button stop">stop</div>
+            <div className="timer-button start" onClick={this.handleStartClick}>start</div>
+            <div className="timer-button stop" onClick={this.handleStopClick}>stop</div>
         </div>    
       </div>
     )
