d3d0f19ac5e9aa9e7d5aa2825519de595b743333
slowbeam <sedwardscode@gmail.com>
2019-03-12 12:03:57 -0400

    more Timer styling

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -5,6 +5,7 @@ export default class Timer extends Component {
   render() {
     return (
       <div className="timer">
+        <div className="timer-counter">00:00</div>
         <div className="timer-buttons">
             <div className="timer-start">start</div>
             <div className="timer-stop">stop</div>
