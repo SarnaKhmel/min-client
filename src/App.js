79ef6fcdad1ca91d45e8705b97be532bea3ef9f2
slowbeam <sedwardscode@gmail.com>
2019-03-18 10:57:55 -0400

    app now renders timers from redux store

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import * as actions from './actions';
+import uuid from 'uuid';
 
 import Timer from './components/Timer/Timer';
 
@@ -10,7 +11,20 @@ import Timer from './components/Timer/Timer';
 class App extends Component {
 
   handleAddTimer = () => {
-    this.props.addTimer({name: "test", currentTime: 200, timerRunning: false});
+    this.props.addTimer({
+      id: uuid(), 
+      name: "test",  
+      currentTime: 0, 
+      intervalNum: null,
+      timerRunning: false,
+      timerHours: "00",
+      timerMinutes: "00",
+      timerSeconds: "00"
+    });
+  }
+
+  renderTimersFromReduxStore = () => {
+    return this.props.timers.map(timer => <Timer key={uuid()} />);
   }
 
   render() {
@@ -20,9 +34,7 @@ class App extends Component {
         <div><h1>min</h1></div>
         <div onClick={this.handleAddTimer}>add a timer</div>
         <div className="timers-container">
-          <Timer />
-          <Timer />
-          <Timer />
+          {this.renderTimersFromReduxStore()}
         </div>   
       </div>
     );
