5adb8a1f6c22c348a5c9158ea230df06103edf62
slowbeam <sedwardscode@gmail.com>
2019-04-08 17:41:33 -0400

    styled login page

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -2,6 +2,7 @@ import React, {useState, useContext} from 'react';
 import {AuthContext} from '../../shared/Auth';
 import {signIn} from '../../../services/auth';
 import {Link} from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
     const [email, setEmail] = useState();
@@ -23,13 +24,21 @@ const Login = () => {
     return (
         <div className="login-page">
             <h1>Login</h1>
-            <form>
-                <input type="text"
+            <form id="login-form">
+                <label name="login-email">
+                    email
+                    <input type="text"
                        placeholder={"Your email..."}
-                       onChange={({target}) => setEmail(target.value)} />
-                <input type="text"
-                       placeholder={"Your password..."}
-                       onChange={({target}) => setPassword(target.value)} />
+                       onChange={({target}) => setEmail(target.value)} 
+                    />
+                </label>
+                
+                <label name="login-pass">
+                    password
+                    <input type="text"
+                        placeholder={"Your password..."}
+                        onChange={({target}) => setPassword(target.value)} />
+                </label>
                 <button onClick={login}>Login</button>
                 <p>don't have an account? <Link to={'/register'}>register</Link></p>
             </form>
