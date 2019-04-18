a05a1b40c2400bf3cc46bd7bfac400c78aa1ab01
slowbeam <sedwardscode@gmail.com>
2019-04-18 17:57:57 -0400

    added conditional drawer for logged in or logged out

--- src/components/shared/MenuDrawer/MenuDrawer.js
+++ src/components/shared/MenuDrawer/MenuDrawer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import PropTypes from 'prop-types';
 import './MenuDrawer.css';
 import { withStyles } from '@material-ui/core/styles';
@@ -8,10 +8,10 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import HamburgerMenu from 'react-hamburger-menu';
-
+import Icon from '@material-ui/core/Icon';
+import {Link} from 'react-router-dom';
+import {AuthContext} from '../../shared/Auth';
 
 const styles = {
   list: {
@@ -22,68 +22,91 @@ const styles = {
   },
 };
 
-class MenuDrawer extends React.Component {
-  state = {
-    open: false
+const MenuDrawer = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const {user} = useContext(AuthContext);
+
+  const toggleDrawer = (boolean) => () => {
+    setIsOpen(boolean);
   };
 
-  toggleDrawer = (boolean) => () => {
-    this.setState({
-      open: boolean
-    });
-  };
+  
+  const { classes } = props;
 
-  render() {
-    const { classes } = this.props;
+  const loggedInList = (
+    <div className={classes.list}>
+      <List>
+        {['multitimer', 'pomodoro'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <Icon className='far fa-clock' /> : <Icon  className='stopwatch fas fa-stopwatch' />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['login', 'register'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon >{index % 2 === 0 ? <Icon className='far fa-user' /> : <Icon className='fas fa-user-plus' />}</ListItemIcon>
+            <Link to={"/register"}><ListItemText primary={text} /></Link>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
-    const sideList = (
-      <div className={classes.list}>
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    );
+  const loggedOutList = (
+    <div className={classes.list}>
+      <List>
+        {['multitimer', 'pomodoro'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <Icon className='far fa-clock' /> : <Icon  className='stopwatch fas fa-stopwatch' />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['login', 'register'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon >{index % 2 === 0 ? <Icon className='far fa-user' /> : <Icon className='fas fa-user-plus' />}</ListItemIcon>
+            <Link to={"/register"}><ListItemText primary={text} /></Link>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
-    return (
-      <div id="mobile-hamburger">
-        <HamburgerMenu
-            isOpen={this.state.open}
-            menuClicked={this.toggleDrawer(true)}
-            width={18}
-            height={15}
-            strokeWidth={2}
-            rotate={0}
-            color='grey'
-            borderRadius={0}
-            animationDuration={0.5}
-        />
-        <Drawer open={this.state.open} onClose={this.toggleDrawer(false)}>
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={this.toggleDrawer(false)}
-            onKeyDown={this.toggleDrawer(false)}
-          >
-            {sideList}
-          </div>
-        </Drawer>
-      </div>
-    );
+  const renderDrawerList = () => {
+    return user ? loggedInList : loggedOutList; 
   }
+
+  return (
+    <div id="mobile-hamburger">
+      <HamburgerMenu
+          isOpen={isOpen}
+          menuClicked={toggleDrawer(true)}
+          width={18}
+          height={15}
+          strokeWidth={2}
+          rotate={0}
+          color='grey'
+          borderRadius={0}
+          animationDuration={0.5}
+      />
+      <Drawer open={isOpen} onClose={toggleDrawer(false)}>
+        <div
+          tabIndex={0}
+          role="button"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          {renderDrawerList()}
+        </div>
+      </Drawer>
+    </div>
+  );
+
 }
 
 MenuDrawer.propTypes = {
